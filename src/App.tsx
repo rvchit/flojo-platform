d5771203e7011408f7fad7f18cc563c80f972bd3
rvchit <rachitb10@gmail.com>
2024-05-18 20:39:29 -0700

    first work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [flowboardName, setFlowboardName] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFlowboardName(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="left-section">
+          <button className="header-button">Athletes</button>
+        </div>
+        <div className="center-section">
+          <button className="header-button">Flowboard</button>
+        </div>
+        <div className="right-section">
+          <button className="header-button">Sign In</button>
+        </div>
       </header>
+      <main className="App-main">
+        <div className="overlay-text">
+          <h1>Help your community unlock their potential through exclusive content</h1>
+        </div>
+        <div className="input-container">
+          <p>Create a flowboard:</p>
+          <div className="input-group">
+            <span>flowboard.me/</span>
+            <input
+              type="text"
+              value={flowboardName}
+              onChange={handleInputChange}
+              placeholder="your-custom-link"
+            />
+          </div>
+        </div>
+        <div className="suggestions">
+          <h1 className="create-button">Have suggestions or concerns email me @rbisht</h1>  
+        </div>
+      </main>
     </div>
   );
 }
